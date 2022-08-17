23dfd93c840570b2a38d31461ee6a8e465bc980b
major <majorx234@googlemail.com>
2022-08-17 18:31:12 +0200

    Add base processing of midi messages after jack receive

--- src/mixer_gui/bin/main.rs
+++ src/mixer_gui/bin/main.rs
@@ -1,16 +1,48 @@
 extern crate jack;
 extern crate wmidi;
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use itertools::izip;
+use std::convert::TryFrom;
 use std::sync::mpsc;
 use std::{thread, time::Duration};
 mod jackmidi;
 use jackmidi::MidiMsg;
 
 fn main() {
+    let (tx_close, rx1_close) = unbounded();
+    let rx2_close = rx1_close.clone();
     let (midi_sender, midi_receiver): (
         std::sync::mpsc::SyncSender<MidiMsg>,
         std::sync::mpsc::Receiver<MidiMsg>,
     ) = mpsc::sync_channel(64);
+    let (tx_mix, rx_mix): (
+        std::sync::mpsc::Sender<(u8, f32)>,
+        std::sync::mpsc::Receiver<(u8, f32)>,
+    ) = mpsc::channel();
+
+    // mididata processing:
+    let midi_thread: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+        while let Ok(m) = midi_receiver.recv() {
+            let bytes: &[u8] = &m.data;
+            let message = wmidi::MidiMessage::try_from(bytes);
+            if let Ok(wmidi::MidiMessage::ControlChange(channel, controlNumber, controlValue)) =
+                message
+            {
+                let volume = u8::from(controlValue) as f32 / 127.0;
+                let mix_channel = u8::from(controlNumber);
+                tx_mix.send((mix_channel, volume)).unwrap();
+            }
+
+            println!("{:?}", m);
+            let mut run = true;
+            run = rx1_close.try_recv().unwrap();
+            if !run {
+                break;
+            }
+        }
+        println!("exit midi thread\n");
+    });
+
     let audio_thread = start_jack_thread(midi_sender);
     audio_thread.join().unwrap();
 }
